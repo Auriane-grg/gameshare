cd2ec7e25e2bb3a828d939136dcf28fcd3cecccb
auriane gregoire <auriane.gregoire@gmail.com>
2021-04-12 17:19:36 +0200

    add pundit to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include Pundit
+
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+	after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  private
+	
+	def skip_pundit? #=> si on est dans un controller de device, on ne met pas un pundit / si on est sur le controller de pages
+		devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+	end
 end
