2b6ddd2a49d2fa27fca2d85ce0f4055ef3861ec1
auriane gregoire <auriane.gregoire@gmail.com>
2021-04-14 17:22:37 +0200

    dashboard : profile info box

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
   def show
     @games = Game.where(user: current_user)
     authorize @games
+    @user = current_user
     # @bookings = Booking.where(user: current_user)
 
     # @review = Review.new
