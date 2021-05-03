cb2d39e298c77375b707ab4df2147691e83b2d25
auriane gregoire <auriane.gregoire@gmail.com>
2021-05-03 20:47:45 +0200

    filter on desktop

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # In Dobble, there is always one and only one identical symbol between each card. Find it and you win! from 6 2 to 6 players 55 cards
+
+# 7 WONDERS DUEL
+# Find 7 Wonders in a version for 2 players! Impose your victory through scientific progress, by capturing your opponent's capital or by building a dominant civilisation.
+# 10 - 99
+# 2 - 8 players
+# 30 min
+# 1 game board, 66 age cards, 7 guild cards, 12 marvel cards, 4 military tokens, 10 progress tokens, 1 conflict counter, 31 coins, 1 scorebook, 1 game rule, 1 game aid
+
+# MONOPOLY
